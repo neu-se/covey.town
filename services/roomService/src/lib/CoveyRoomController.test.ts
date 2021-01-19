ec0e1faf55f187f3ce35cdbe9d6d676d5f84eb15
Jonathan Bell <jon@jonbell.net>
2021-01-19 12:06:30 -0500

    Add an editorConfig in the roomService directory too to make it easier when exporting; add a test to be sure that roomID and userName are passsed property to the video token method

--- services/roomService/src/lib/CoveyRoomController.test.ts
+++ services/roomService/src/lib/CoveyRoomController.test.ts
@@ -1,7 +1,20 @@
 import { nanoid } from 'nanoid';
 import CoveyRoomsStore from './CoveyRoomsStore';
+import TwilioVideo from './TwilioVideo';
+import Player from '../types/Player';
 
+jest.mock('./TwilioVideo');
+
+const mockGetTokenForRoom = jest.fn();
+// eslint-disable-next-line
+// @ts-ignore it's a mock
+TwilioVideo.getInstance = () => ({
+  getTokenForRoom: mockGetTokenForRoom,
+});
 describe('CoveyRoomController', () => {
+  beforeEach(() => {
+    mockGetTokenForRoom.mockClear();
+  });
   it('should set the coveyRoomID property', () => {
     const roomsStore = CoveyRoomsStore.getInstance();
     const roomName = nanoid();
@@ -9,4 +22,20 @@ describe('CoveyRoomController', () => {
     expect(roomController.coveyRoomID)
       .toBe(roomName);
   });
+  it('should use the coveyRoomID and userName properties when requesting a video token',
+    async () => {
+      const roomsStore = CoveyRoomsStore.getInstance();
+      const roomName = nanoid();
+      const roomController = roomsStore.getControllerForRoom(roomName);
+      const playerID = nanoid();
+      await roomController.addPlayer(new Player(playerID));
+      expect(mockGetTokenForRoom.mock.calls.length)
+        .toBe(1); // should have called getToken once
+      // should have passed the room name to getToken
+      expect(mockGetTokenForRoom.mock.calls[0][0])
+        .toBe(roomName);
+      // should have passed the player id to getToken
+      expect(mockGetTokenForRoom.mock.calls[0][1])
+        .toBe(playerID);
+    });
 });
