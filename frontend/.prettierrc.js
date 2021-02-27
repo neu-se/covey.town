cf43bcd4b490c7f8e93eddef3458464e03a853c8
Jonathan Bell <jon@jonbell.net>
2021-02-27 12:07:58 -0500

    formatting updates

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  proseWrap: 'always',
+  htmlWhitespaceSensitivity: 'ignore',
+  endOfLine: 'lf',
+};
