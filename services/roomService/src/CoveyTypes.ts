732b278cfb5503a95fa7ca934f67612ca257fa33
anikarabenhorst <anikacchase@gmail.com>
2021-03-15 19:27:39 -0400

    feat: add MessageType and Message to CoveyTypes

--- services/roomService/src/CoveyTypes.ts
+++ services/roomService/src/CoveyTypes.ts
@@ -1,3 +1,5 @@
+import Player from "./types/Player";
+
 export type Direction = 'front' | 'back' | 'left' | 'right';
 export type UserLocation = {
   x: number;
@@ -7,3 +9,16 @@ export type UserLocation = {
 };
 export type CoveyTownList = { friendlyName: string; coveyTownID: string; currentOccupancy: number; maximumOccupancy: number }[];
 
+export enum MessageType {
+  DirectMessage,
+  ProximityMessage,
+  TownMessage
+};
+
+export type Message = {
+  user: Player;
+  messageContent: string;
+  timestamp: string;
+  type: MessageType;
+  directMessageId: string | null;
+}
