cdcbcb3dbcaaa5dd7865ed982df98e3691452717
Jonathan Bell <jon@jonbell.net>
2021-03-19 15:22:49 -0400

    Update to newest version of Phaser, and add an example of interactive objects: transporters.
    Transporters are defined in Tiled as objects (objects with name "transporter", and a custom property called "target" which is an object reference to the location that the transporter should jump to). Place a text label inside of the transporter area (with name "label" and whatever text you'd like). Then, when a user walks into that transporter area and presses the spacebar, they will be transported to the location that the transporter specifies as a target. See code in WorldMap.tsx added in this commit.

--- frontend/src/components/world/WorldMap.tsx
+++ frontend/src/components/world/WorldMap.tsx
@@ -93,7 +93,7 @@ class CoveyGameScene extends Phaser.Scene {
           .setOffset(0, 24);
         const label = this.add.text(0, 0, myPlayer.userName, {
           font: '18px monospace',
-          fill: '#000000',
+          color: '#000000',
           backgroundColor: '#ffffff',
         });
         myPlayer.label = label;
@@ -114,23 +114,23 @@ class CoveyGameScene extends Phaser.Scene {
   }
 
   getNewMovementDirection() {
-    if(this.cursors.find(keySet => keySet.left?.isDown)) {
+    if (this.cursors.find(keySet => keySet.left?.isDown)) {
       return 'left';
     }
-    if(this.cursors.find(keySet => keySet.right?.isDown)) {
+    if (this.cursors.find(keySet => keySet.right?.isDown)) {
       return 'right';
     }
-    if(this.cursors.find(keySet => keySet.down?.isDown)) {
+    if (this.cursors.find(keySet => keySet.down?.isDown)) {
       return 'front';
     }
-    if(this.cursors.find(keySet => keySet.up?.isDown)) {
+    if (this.cursors.find(keySet => keySet.up?.isDown)) {
       return 'back';
     }
     return undefined;
   }
 
   update() {
-    if(this.paused){
+    if (this.paused) {
       return;
     }
     if (this.player && this.cursors) {
@@ -142,7 +142,7 @@ class CoveyGameScene extends Phaser.Scene {
       body.setVelocity(0);
 
       const primaryDirection = this.getNewMovementDirection();
-      switch(primaryDirection){
+      switch (primaryDirection) {
         case 'left':
           body.setVelocityX(-speed);
           this.player.sprite.anims.play('misa-left-walk', true);
@@ -172,7 +172,6 @@ class CoveyGameScene extends Phaser.Scene {
           } else if (prevVelocity.y > 0) this.player.sprite.setTexture('atlas', 'misa-front');
           break;
       }
-
 
       // Normalize and scale the velocity so that player can't move faster along a diagonal
       this.player.sprite.body.velocity.normalize()
@@ -212,10 +211,10 @@ class CoveyGameScene extends Phaser.Scene {
 
     // Parameters: layer name (or index) from Tiled, tileset, x, y
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const belowLayer = map.createStaticLayer('Below Player', tileset, 0, 0);
-    const worldLayer = map.createStaticLayer('World', tileset, 0, 0);
+    const belowLayer = map.createLayer('Below Player', tileset, 0, 0);
+    const worldLayer = map.createLayer('World', tileset, 0, 0);
     worldLayer.setCollisionByProperty({ collides: true });
-    const aboveLayer = map.createStaticLayer('Above Player', tileset, 0, 0);
+    const aboveLayer = map.createLayer('Above Player', tileset, 0, 0);
     /* By default, everything gets depth sorted on the screen in the order we created things.
      Here, we want the "Above Player" layer to sit on top of the player, so we explicitly give
      it a depth. Higher depths will sit on top of lower depth objects.
@@ -228,6 +227,53 @@ class CoveyGameScene extends Phaser.Scene {
       (obj) => obj.name === 'Spawn Point') as unknown as
       Phaser.GameObjects.Components.Transform;
 
+
+    // Find all of the transporters, add them to the physics engine
+    const transporters = map.createFromObjects('Objects',
+      { name: 'transporter' })
+    this.physics.world.enable(transporters);
+
+    // For each of the transporters (rectangle objects), we need to tweak their location on the scene
+    // for reasons that are not obvious to me, but this seems to work. We also set them to be invisible
+    // but for debugging, you can comment out that line.
+    transporters.forEach(transporter => {
+        const sprite = transporter as Phaser.GameObjects.Sprite;
+        sprite.y += 2 * sprite.height; // Phaser and Tiled seem to disagree on which corner is y
+        sprite.setVisible(false); // Comment this out to see the transporter rectangles drawn on
+                                  // the map
+      }
+    );
+
+    const labels = map.filterObjects('Objects',(obj)=>obj.name==='label');
+    labels.forEach(label => {
+      if(label.x && label.y){
+        this.add.text(label.x, label.y, label.text.text, {
+          color: '#FFFFFF',
+          backgroundColor: '#000000',
+        })
+      }
+    });
+
+
+
+    const cursorKeys = this.input.keyboard.createCursorKeys();
+    this.cursors.push(cursorKeys);
+    this.cursors.push(this.input.keyboard.addKeys({
+      'up': Phaser.Input.Keyboard.KeyCodes.W,
+      'down': Phaser.Input.Keyboard.KeyCodes.S,
+      'left': Phaser.Input.Keyboard.KeyCodes.A,
+      'right': Phaser.Input.Keyboard.KeyCodes.D
+    }, false) as Phaser.Types.Input.Keyboard.CursorKeys);
+    this.cursors.push(this.input.keyboard.addKeys({
+      'up': Phaser.Input.Keyboard.KeyCodes.H,
+      'down': Phaser.Input.Keyboard.KeyCodes.J,
+      'left': Phaser.Input.Keyboard.KeyCodes.K,
+      'right': Phaser.Input.Keyboard.KeyCodes.L
+    }, false) as Phaser.Types.Input.Keyboard.CursorKeys);
+
+
+
+
     // Create a sprite with physics enabled via the physics system. The image used for the sprite
     // has a bit of whitespace, so I'm using setSize & setOffset to control the size of the
     // player's body.
@@ -237,7 +283,7 @@ class CoveyGameScene extends Phaser.Scene {
       .setOffset(0, 24);
     const label = this.add.text(spawnPoint.x, spawnPoint.y - 20, '(You)', {
       font: '18px monospace',
-      fill: '#000000',
+      color: '#000000',
       // padding: {x: 20, y: 10},
       backgroundColor: '#ffffff',
     });
@@ -245,6 +291,32 @@ class CoveyGameScene extends Phaser.Scene {
       sprite,
       label
     };
+
+    /* Configure physics overlap behavior for when the player steps into
+    a transporter area. If you enter a transporter and press 'space', you'll
+    transport to the location on the map that is referenced by the 'target' property
+    of the transporter.
+     */
+    this.physics.add.overlap(sprite, transporters,
+      (overlappingObject, transporter)=>{
+      if(cursorKeys.space.isDown && this.player){
+        // In the tiled editor, set the 'target' to be an *object* pointer
+        // Here, we'll see just the ID, then find the object by ID
+        const transportTargetID = transporter.getData('target') as number;
+        const target = map.findObject('Objects', obj => (obj as unknown as Phaser.Types.Tilemaps.TiledObject).id === transportTargetID);
+        if(target && target.x && target.y && this.lastLocation){
+          // Move the player to the target, update lastLocation and send it to other players
+          this.player.sprite.x = target.x;
+          this.player.sprite.y = target.y;
+          this.lastLocation.x = target.x;
+          this.lastLocation.y = target.y;
+          this.emitMovement(this.lastLocation);
+        }
+        else{
+          throw new Error(`Unable to find target object ${target}`);
+        }
+      }
+    })
 
     this.emitMovement({
       rotation: 'front',
@@ -310,27 +382,13 @@ class CoveyGameScene extends Phaser.Scene {
     camera.startFollow(this.player.sprite);
     camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
-    this.cursors.push(this.input.keyboard.createCursorKeys());
-    this.cursors.push(this.input.keyboard.addKeys({up:Phaser.Input.Keyboard.KeyCodes.W,down:Phaser.Input.Keyboard.KeyCodes.S,left:Phaser.Input.Keyboard.KeyCodes.A,right:Phaser.Input.Keyboard.KeyCodes.D}));
-    this.cursors.push(this.input.keyboard.addKeys({up:Phaser.Input.Keyboard.KeyCodes.H,down:Phaser.Input.Keyboard.KeyCodes.J,left:Phaser.Input.Keyboard.KeyCodes.K,right:Phaser.Input.Keyboard.KeyCodes.L}));
-
-    this.input.keyboard.removeCapture([
-      Phaser.Input.Keyboard.KeyCodes.W,
-      Phaser.Input.Keyboard.KeyCodes.A,
-      Phaser.Input.Keyboard.KeyCodes.S,
-      Phaser.Input.Keyboard.KeyCodes.D,
-      Phaser.Input.Keyboard.KeyCodes.H,
-      Phaser.Input.Keyboard.KeyCodes.J,
-      Phaser.Input.Keyboard.KeyCodes.K,
-      Phaser.Input.Keyboard.KeyCodes.L,
-    ]);
 
 
     // Help text that has a "fixed" position on the screen
     this.add
-      .text(16, 16, `Arrow keys to move\nCurrent town: ${this.video.townFriendlyName} (${this.video.coveyTownID})`, {
+      .text(16, 16, `Arrow keys to move, space to transport\nCurrent town: ${this.video.townFriendlyName} (${this.video.coveyTownID})`, {
         font: '18px monospace',
-        fill: '#000000',
+        color: '#000000',
         padding: {
           x: 20,
           y: 10
@@ -352,7 +410,7 @@ class CoveyGameScene extends Phaser.Scene {
     this.paused = true;
   }
 
-  resume(){
+  resume() {
     this.paused = false;
   }
 }
