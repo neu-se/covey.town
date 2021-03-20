524d6a98079c3f18007a5817a3d061cd38df2df9
Jonathan Bell <jon@jonbell.net>
2021-03-20 09:58:48 -0400

    Integrate HW4 solution + refactored tests

--- frontend/src/components/Login/TownSettings.tsx
+++ frontend/src/components/Login/TownSettings.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import {
   Button,
@@ -22,9 +22,12 @@ import useCoveyAppState from '../../hooks/useCoveyAppState';
 import useMaybeVideo from '../../hooks/useMaybeVideo';
 
 const TownSettings: React.FunctionComponent = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const { apiClient, currentTownID, currentTownFriendlyName, currentTownIsPubliclyListed } = useCoveyAppState();
+  const {isOpen, onOpen, onClose} = useDisclosure()
   const video = useMaybeVideo()
+  const {apiClient, currentTownID, currentTownFriendlyName, currentTownIsPubliclyListed} = useCoveyAppState();
+  const [friendlyName, setFriendlyName] = useState<string>(currentTownFriendlyName);
+  const [isPubliclyListed, setIsPubliclyListed] = useState<boolean>(currentTownIsPubliclyListed);
+  const [roomUpdatePassword, setRoomUpdatePassword] = useState<string>('');
 
   const openSettings = useCallback(()=>{
     onOpen();
@@ -37,14 +40,45 @@ const TownSettings: React.FunctionComponent = () => {
   }, [onClose, video]);
 
   const toast = useToast()
-  const processUpdates = async () => {
-    toast({
-      title: 'debug, remove this when you implement the functionality',
-      description: `
-      current status: town ID: ${currentTownID}, is public: ${currentTownIsPubliclyListed},
-      friendlyName: ${currentTownFriendlyName}`,
-      status: 'info'
-    })
+  const processUpdates = async (action: string) =>{
+    if(action === 'delete'){
+      try{
+        await apiClient.deleteTown({coveyTownID: currentTownID,
+          coveyTownPassword: roomUpdatePassword});
+        toast({
+          title: 'Town deleted',
+          status: 'success'
+        })
+        closeSettings();
+      }catch(err){
+        toast({
+          title: 'Unable to delete town',
+          description: err.toString(),
+          status: 'error'
+        });
+      }
+    }else {
+      try {
+        await apiClient.updateTown({
+          coveyTownID: currentTownID,
+          coveyTownPassword: roomUpdatePassword,
+          friendlyName,
+          isPubliclyListed
+        });
+        toast({
+          title: 'Town updated',
+          description: 'To see the updated town, please exit and re-join this town',
+          status: 'success'
+        })
+        closeSettings();
+      }catch(err){
+        toast({
+          title: 'Unable to update town',
+          description: err.toString(),
+          status: 'error'
+        });
+      }
+    }
   };
 
   return <>
@@ -56,34 +90,28 @@ const TownSettings: React.FunctionComponent = () => {
       <ModalContent>
         <ModalHeader>Edit town {currentTownFriendlyName} ({currentTownID})</ModalHeader>
         <ModalCloseButton/>
-        <form onSubmit={(ev) => {
-          ev.preventDefault();
-          processUpdates();
-        }}>
+        <form onSubmit={(ev)=>{ev.preventDefault(); processUpdates('edit')}}>
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel htmlFor='friendlyName'>Friendly Name</FormLabel>
-              <Input id='friendlyName' placeholder="Friendly Name" name="friendlyName" />
+              <Input id='friendlyName' placeholder="Friendly Name" name="friendlyName" value={friendlyName} onChange={(ev)=>setFriendlyName(ev.target.value)} />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel htmlFor='isPubliclyListed'>Publicly Listed</FormLabel>
-              <Checkbox id="isPubliclyListed" name="isPubliclyListed"/>
+              <Checkbox id="isPubliclyListed" name="isPubliclyListed"  isChecked={isPubliclyListed} onChange={(e)=>setIsPubliclyListed(e.target.checked)} />
             </FormControl>
             <FormControl isRequired>
               <FormLabel htmlFor="updatePassword">Town Update Password</FormLabel>
-              <Input data-testid="updatePassword" id="updatePassword" placeholder="Password"
-                     name="password" type="password"/>
+              <Input data-testid="updatePassword" id="updatePassword" placeholder="Password" name="password" type="password" value={roomUpdatePassword} onChange={(e)=>setRoomUpdatePassword(e.target.value)} />
             </FormControl>
           </ModalBody>
 
           <ModalFooter>
-            <Button data-testid='deletebutton' colorScheme="red" mr={3} value="delete">
+            <Button data-testid='deletebutton' colorScheme="red" mr={3} value="delete" name='action1' onClick={()=>processUpdates('delete')}>
               Delete
             </Button>
-            <Button data-testid='updatebutton' colorScheme="blue" type="submit" mr={3}
-                    onClick={() => processUpdates()}
-                    value="update">
+            <Button data-testid='updatebutton' colorScheme="blue" mr={3} value="update" name='action2' onClick={()=>processUpdates('edit')}>
               Update
             </Button>
             <Button onClick={closeSettings}>Cancel</Button>
@@ -93,5 +121,6 @@ const TownSettings: React.FunctionComponent = () => {
     </Modal>
   </>
 }
+
 
 export default TownSettings;
---
+++ services/roomService/src/client/CoveyTownsSocket.test.ts
@@ -0,0 +1,136 @@
+import Express from 'express';
+import CORS from 'cors';
+import http from 'http';
+import { nanoid } from 'nanoid';
+import { AddressInfo } from 'net';
+import { promisify } from 'util';
+import io from 'socket.io';
+import * as TestUtils from './TestUtils';
+
+import { UserLocation } from '../CoveyTypes';
+import TownsServiceClient from './TownsServiceClient';
+import addTownRoutes from '../router/towns';
+
+type TestTownData = {
+  friendlyName: string, coveyTownID: string,
+  isPubliclyListed: boolean, townUpdatePassword: string
+};
+
+describe('TownServiceApiSocket', () => {
+  let server: http.Server;
+  let apiClient: TownsServiceClient;
+
+  async function createTownForTesting(friendlyNameToUse?: string, isPublic = false): Promise<TestTownData> {
+    const friendlyName = friendlyNameToUse !== undefined ? friendlyNameToUse :
+      `${isPublic ? 'Public' : 'Private'}TestingTown=${nanoid()}`;
+    const ret = await apiClient.createTown({
+      friendlyName,
+      isPubliclyListed: isPublic,
+    });
+    return {
+      friendlyName,
+      isPubliclyListed: isPublic,
+      coveyTownID: ret.coveyTownID,
+      townUpdatePassword: ret.coveyTownPassword,
+    };
+  }
+
+  let socketServer: io.Server;
+  beforeAll(async () => {
+    const app = Express();
+    app.use(CORS());
+    server = http.createServer(app);
+
+    socketServer = addTownRoutes(server, app);
+    server.listen();
+    const address = server.address() as AddressInfo;
+
+    apiClient = new TownsServiceClient(`http://127.0.0.1:${address.port}`);
+  });
+  afterAll(async () => {
+    server.close();
+    TestUtils.cleanupSockets();
+  });
+  afterEach(() => {
+    TestUtils.cleanupSockets();
+  });
+  it('Rejects invalid CoveyTownIDs, even if otherwise valid session token', async () => {
+    const town = await createTownForTesting();
+    const joinData = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const {socketDisconnected} = TestUtils.createSocketClient(server, joinData.coveySessionToken, nanoid());
+    await socketDisconnected;
+  });
+  it('Rejects invalid session tokens, even if otherwise valid town id', async () => {
+    const town = await createTownForTesting();
+    await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const {socketDisconnected} = TestUtils.createSocketClient(server, nanoid(), town.coveyTownID);
+    await socketDisconnected;
+  });
+  it('Dispatches movement updates to all clients in the same town', async () => {
+    const town = await createTownForTesting();
+    const joinData = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const joinData2 = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const joinData3 = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const socketSender = TestUtils.createSocketClient(server, joinData.coveySessionToken, town.coveyTownID).socket;
+    const {playerMoved} = TestUtils.createSocketClient(server, joinData2.coveySessionToken, town.coveyTownID);
+    const {playerMoved: playerMoved2} = TestUtils.createSocketClient(server, joinData3.coveySessionToken, town.coveyTownID);
+    const newLocation: UserLocation = {x: 100, y: 100, moving: true, rotation: 'back'};
+    socketSender.emit('playerMovement', newLocation);
+    const [movedPlayer, otherMovedPlayer]= await Promise.all([playerMoved, playerMoved2]);
+    expect(movedPlayer.location).toMatchObject(newLocation);
+    expect(otherMovedPlayer.location).toMatchObject(newLocation);
+  });
+  it('Invalidates the user session after disconnection', async () => {
+    // This test will timeout if it fails - it will never reach the expectation
+    const town = await createTownForTesting();
+    const joinData = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const {socket, socketConnected} = TestUtils.createSocketClient(server, joinData.coveySessionToken, town.coveyTownID);
+    await socketConnected;
+    socket.close();
+    const {socket: secondTryWithSameToken, socketDisconnected: secondSocketDisconnected} = TestUtils.createSocketClient(server, joinData.coveySessionToken, town.coveyTownID);
+    await secondSocketDisconnected;
+    expect(secondTryWithSameToken.disconnected).toBe(true);
+  });
+  it('Informs all new players when a player joins', async () => {
+    const town = await createTownForTesting();
+    const joinData = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const joinData2 = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const {socketConnected, newPlayerJoined} = TestUtils.createSocketClient(server, joinData.coveySessionToken, town.coveyTownID);
+    const {
+      socketConnected: connectPromise2,
+      newPlayerJoined: newPlayerPromise2,
+    } = TestUtils.createSocketClient(server, joinData2.coveySessionToken, town.coveyTownID);
+    await Promise.all([socketConnected, connectPromise2]);
+    const newJoinerName = nanoid();
+
+    await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: newJoinerName});
+    expect((await newPlayerJoined)._userName).toBe(newJoinerName);
+    expect((await newPlayerPromise2)._userName).toBe(newJoinerName);
+
+  });
+  it('Informs all players when a player disconnects', async () => {
+    const town = await createTownForTesting();
+    const joinData = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const joinData2 = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const userWhoLeaves = nanoid();
+    const joinDataWhoLeaves = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: userWhoLeaves});
+    const {socketConnected, playerDisconnected} = TestUtils.createSocketClient(server, joinData.coveySessionToken, town.coveyTownID);
+    const {socketConnected: connectPromise2, playerDisconnected: playerDisconnectPromise2} = TestUtils.createSocketClient(server, joinData2.coveySessionToken, town.coveyTownID);
+    const {socket: socketWhoLeaves, socketConnected: connectPromise3} = TestUtils.createSocketClient(server, joinDataWhoLeaves.coveySessionToken, town.coveyTownID);
+    await Promise.all([socketConnected, connectPromise2, connectPromise3]);
+    socketWhoLeaves.close();
+    expect((await playerDisconnected)._userName).toBe(userWhoLeaves);
+    expect((await playerDisconnectPromise2)._userName).toBe(userWhoLeaves);
+
+  });
+  it('Informs all players when the town is destroyed', async () => {
+    const town = await createTownForTesting();
+    const joinData = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const joinData2 = await apiClient.joinTown({coveyTownID: town.coveyTownID, userName: nanoid()});
+    const {socketDisconnected, socketConnected} = TestUtils.createSocketClient(server, joinData.coveySessionToken, town.coveyTownID);
+    const {socketDisconnected: disconnectPromise2, socketConnected: connectPromise2} = TestUtils.createSocketClient(server, joinData2.coveySessionToken, town.coveyTownID);
+    await Promise.all([socketConnected, connectPromise2]);
+    await apiClient.deleteTown({coveyTownID: town.coveyTownID, coveyTownPassword: town.townUpdatePassword});
+    await Promise.all([socketDisconnected, disconnectPromise2]);
+  });
+});
