ce77f738a76adb738a0af331e0e24526e025b7b4
eric-benny <59672346+eric-benny@users.noreply.github.com>
2021-03-20 13:33:18 -0400

    Player map migration logic
    Logic for migrating a player to a new map, and notifying other players of the move

--- services/roomService/src/lib/CoveyTownController.ts
+++ services/roomService/src/lib/CoveyTownController.ts
@@ -1,5 +1,5 @@
 import { customAlphabet, nanoid } from 'nanoid';
-import { UserLocation } from '../CoveyTypes';
+import { CoveyTownRoomID, UserLocation } from '../CoveyTypes';
 import CoveyTownListener from '../types/CoveyTownListener';
 import Player from '../types/Player';
 import PlayerSession from '../types/PlayerSession';
@@ -16,6 +16,7 @@ export default class CoveyTownController {
   get capacity(): number {
     return this._capacity;
   }
+
   set isPubliclyListed(value: boolean) {
     this._isPubliclyListed = value;
   }
@@ -118,6 +119,16 @@ export default class CoveyTownController {
   updatePlayerLocation(player: Player, location: UserLocation): void {
     player.updateLocation(location);
     this._listeners.forEach((listener) => listener.onPlayerMoved(player));
+  }
+
+  /**
+   * Updates the map of a player within the town
+   * @param player Player to update location for
+   * @param map_id New location for this player
+   */
+  updatePlayerMap(player: Player, mapId: CoveyTownRoomID): void {
+    player.updateMapId(mapId);
+    this._listeners.forEach((listener) => listener.onPlayerMapChange(player));
   }
 
   /**
