8183129ebd3d5356a896e47c29b85840692aea69
calvinchancan <chan.calv@husky.neu.edu>
2021-03-20 13:40:55 -0400

    Add Twilio Starter for Chat Backend.

---
+++ chatservice/app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+// Node/Express
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const router = require('./src/router');
+const syncServiceDetails = require('./src/sync_service_details');
+
+// Create Express webapp
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Add body parser for Notify device registration
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(router);
+
+// Get Sync Service Details for lazy creation of default service if needed
+syncServiceDetails();
+
+// Create http server and run it
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+server.listen(port, function() {
+  console.log('Express server running on *:' + port);
+});
+
+module.exports = app;
