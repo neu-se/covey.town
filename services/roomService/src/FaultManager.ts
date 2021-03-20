2303112263894ff3b5bb9ba994d29cf757712ecd
eric-benny <59672346+eric-benny@users.noreply.github.com>
2021-03-20 13:02:45 -0400

    Test Setup
    Reintroduce backend tests

---
+++ services/roomService/src/FaultManager.ts
@@ -0,0 +1,10 @@
+// Do not touch this file - the autograder will use a different implementation of this file
+
+// @ts-ignore
+export function ConfigureTest(testID: string): string[] {
+  return ['No fault'];
+}
+
+// @ts-ignore
+export function StartTest(testID: string): void {
+}
