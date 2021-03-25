c34b6b5b160038c99f23bf2c1272793845d4d428
mdhanapal1812 <muthouazhagi1812@gmail.com>
2021-03-25 18:27:16 -0400

    committing before changing to masters

--- services/roomService/src/data/models/users/user.model.server.ts
+++ services/roomService/src/data/models/users/user.model.server.ts
@@ -1,4 +1,3 @@
-/* let mongooseD = require('mongoose'); */
 import mongoose from 'mongoose';
 import { userSchema } from './user.schema.server';
 export const userModel = mongoose.model('User', userSchema);
