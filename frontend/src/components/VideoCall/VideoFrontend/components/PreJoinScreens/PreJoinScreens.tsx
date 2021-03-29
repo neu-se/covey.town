842b4bd191dac45de3dbea95e9e1f9b86ce227d2
Tran <pham.tr@northeastern.edu>
2021-03-29 13:56:16 -0400

    added login and create account button to the main interface

--- frontend/src/components/VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens.tsx
+++ frontend/src/components/VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens.tsx
@@ -4,10 +4,14 @@ import DeviceSelectionScreen from './DeviceSelectionScreen/DeviceSelectionScreen
 import IntroContainer from '../IntroContainer/IntroContainer';
 import { TownJoinResponse } from '../../../../../classes/TownsServiceClient';
 import TownSelection from '../../../../Login/TownSelection';
+import LoginPopUp from '../../../../Login/LoginPopUp';
+import CreateAccountPopUp from '../../../../Login/CreateAccountPopUp';
 
 export default function PreJoinScreens(props: { doLogin: (initData: TownJoinResponse) => Promise<boolean>; setMediaError?(error: Error): void }) {
   return (
     <IntroContainer>
+      <LoginPopUp  />
+      <CreateAccountPopUp />
       <Heading as="h2" size="xl">Welcome to Covey.Town!</Heading>
       <Text p="4">
         Covey.Town is a social platform that integrates a 2D game-like metaphor with video chat.
