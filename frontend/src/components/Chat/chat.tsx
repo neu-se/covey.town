47b7a1dff58cb1d52e0ba3806f9387a951002fb0
Likhit M <likhit.m84@gmail.com>
2021-03-30 14:29:09 -0400

    Chat window

--- frontend/src/components/Chat/chat.tsx
+++ frontend/src/components/Chat/chat.tsx
@@ -1,8 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import {
+    Box,
+    Button,
+    Checkbox,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+    Table,
+    TableCaption,
+    Tbody,
+    Td,
+    Th,
+    Thead,
+    Tr,
+    useToast
+} from '@chakra-ui/react';
 import { nanoid } from 'nanoid';
 
 import Client from 'twilio-chat';
+import { Channel } from 'twilio-chat/lib/channel';
 import useCoveyAppState from '../../hooks/useCoveyAppState';
+
 
 
 
@@ -19,36 +40,67 @@ export default function ChatWindow(): JSX.Element {
     const { videoToken, broadcastChannelSID } = useCoveyAppState();
 
     const [client, setClient] = useState<Client>();
+    const [channel, setChannel] = useState<Channel>();
+
+    const [message, setMessage] = useState<string>('');
 
     useEffect(() => {
-        console.log("Helloooooooooooooo")
         Client.create(videoToken).then(newClient => {
             setClient(newClient)
-            console.log("getChannel")
-            client?.getChannelBySid(broadcastChannelSID).then(channel => {
-                console.log(`channel name ${channel.uniqueName} ${channel.friendlyName}`)
-                channel.join().then(channel1 =>
-                    channel1.sendMessage(`hello ${nanoid(5)}`).then(num => {
-                        channel1.getMessages().then(messages => {
-                            const totalMessages = messages.items.length;
-                            for (let i = 0; i < totalMessages; i += 1) {
-                                const message = messages.items[i];
-                                console.log(`Author: + ${message.author}`);
-                            }
-                            console.log(`Total Messages:' + ${totalMessages}`);
-                        })
-                    })
+            newClient.getChannelBySid(broadcastChannelSID).then(broadcastChannel => {
+                setChannel(broadcastChannel)
+                broadcastChannel.join().then(joinedChannel => joinedChannel.on('messageAdded', (newMessage) => {
+                    console.log(`Author: + ${newMessage.author}`);
+                    console.log(`message:' + ${newMessage.body}`);
 
-                )
+                }))
             }
+                // newClient?.getChannelBySid(broadcastChannelSID).then(channel => {
+                //     console.log(`channel name ${channel.uniqueName} ${channel.friendlyName}`)
+                //     channel.join().then(channel1 =>
+                //         channel1.sendMessage(`hello ${nanoid(5)}`).then(num => {
+                //             channel1.getMessages().then(messages => {
+                //                 const totalMessages = messages.items.length;
+                //                 for (let i = 0; i < totalMessages; i += 1) {
+                //                     const message = messages.items[i];
+                //                     console.log(`Author: + ${message.author}`);
+                //                 }
+                //                 console.log(`Total Messages:' + ${totalMessages}`);
+                //             })
+                //         })
+
+                //     )
+                // }
+                // )
+
             )
         }
 
 
         )
-
     }, [videoToken, broadcastChannelSID])
 
-    return <h1>Hello ${videoToken} ${broadcastChannelSID}</h1>
 
+    const handleMessage = async () => {
+        channel?.sendMessage(message).then(num => setMessage(''))
+    }
+
+    return <div>
+        <form>
+            <Stack>
+                <Box p="4" borderWidth="1px" borderRadius="lg">
+
+                    <FormControl>
+                        <FormLabel htmlFor="name">Message</FormLabel>
+                        <Input autoFocus name="name" placeholder="Your name"
+                            value={message}
+                            onChange={event => setMessage(event.target.value)}
+                        />
+                    </FormControl>
+                    <button type='submit' onClick={handleMessage}>Send</button>
+                </Box>
+            </Stack>
+        </form>
+
+    </div>
 }
