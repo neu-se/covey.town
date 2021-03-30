e385fc3c7b94eccd3ddc636cc6e6168305fd7e0f
Tran <pham.tr@northeastern.edu>
2021-03-29 21:51:13 -0400

    switch order create account/login button

--- frontend/src/components/VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens.tsx
+++ frontend/src/components/VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens.tsx
@@ -10,9 +10,10 @@ import CreateAccountPopUp from '../../../../Login/CreateAccountPopUp';
 export default function PreJoinScreens(props: { doLogin: (initData: TownJoinResponse) => Promise<boolean>; setMediaError?(error: Error): void }) {
   return (
     <IntroContainer>
-      <LoginPopUp  />
+      
       <CreateAccountPopUp />
-      <Heading as="h2" size="xl">Welcome to Covey.Town!</Heading>
+      <LoginPopUp  />
+      <Heading as="h2" size="xl" >Welcome to Covey.Town!</Heading>
       <Text p="4">
         Covey.Town is a social platform that integrates a 2D game-like metaphor with video chat.
         To get started, setup your camera and microphone, choose a username, and then create a new town
