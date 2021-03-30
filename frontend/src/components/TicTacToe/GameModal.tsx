c9646758b07681121f54f0580455dd5496b80fd7
Harumi Jang <harumijang@Harumis-MacBook-Pro.local>
2021-03-30 17:27:57 -0400

    added game modal

---
+++ frontend/src/components/TicTacToe/GameModal.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback, useState } from 'react';
+
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast
+} from '@chakra-ui/react';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import useMaybeVideo from '../../hooks/useMaybeVideo';
+
+
+const GameModal: React.FunctionComponent = () => {
+  const {isOpen, onOpen, onClose} = useDisclosure()
+  const video = useMaybeVideo()
+
+  const openGame = useCallback(()=>{
+    onOpen();
+    video?.pauseGame();
+  }, [onOpen, video]);
+
+  if (video) {
+    video.openGameModal = openGame;
+  }
+
+  const closeGame = useCallback(()=>{
+    onClose();
+    video?.unPauseGame();
+  }, [onClose, video]);
+
+
+  return <>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay/>
+      <ModalContent>
+        <ModalHeader>TIC TAC TOE</ModalHeader>
+        <Button onClick={closeGame}>Close</Button>
+          <ModalBody pb={6}/>
+      </ModalContent>
+    </Modal>
+  </>
+}
+
+
+export default GameModal;
