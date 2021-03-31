3d24a2e4716f3332eaf4603ebf78ebadae4e9973
mirandadayadkins <mirandaday16@gmail.com>
2021-03-31 12:43:25 -0400

    starting controller implementation

--- services/roomService/src/games/TicTacToeGame.ts
+++ services/roomService/src/games/TicTacToeGame.ts
@@ -15,4 +15,21 @@ export default class TicTacToeGame implements IGame {
   constructor() {
     this._id = nanoid();
   }
+
+  gameState: string;
+  player1ID: string;
+  player2ID: string;
+
+  finishGame(gameState: string, player1ID: string, player2ID: string): void {
+  }
+
+  initializeGame(): void {
+  }
+
+  isGameOver(): boolean {
+    return false;
+  }
+
+  move(move: TicMove | TTLPlayer1Move | TTLPlayer2Move | HangmanPlayer1Move | HangmanPlayer2Move): void {
+  }
 }
