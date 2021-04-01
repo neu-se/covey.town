192ba90dfc6e7fc5e5346f775adc395420647b15
mohitchandarana <chandarana.m@husky.neu.edu>
2021-04-01 04:14:15 -0400

    Added and configured graphql and set up examples

---
+++ frontend/src/components/GQLExample/GQLExample.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+query GetBooks {
+  books {
+    title
+    author
+  }
+}
+`;
+
+export default function GQLExample() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  console.log('HERE!', loading, data);
+
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return data.books.map(({ title, author }:any) => (
+    <div key={title}>
+      <p>
+        {title}: {author}
+      </p>
+    </div>
+  ));
+}
