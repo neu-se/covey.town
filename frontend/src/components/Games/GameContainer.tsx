94e86bb9f15cdd69cc684561a108c5e1f30652a5
mirandadayadkins <mirandaday16@gmail.com>
2021-04-01 16:20:23 -0400

    starter code for game container

--- frontend/src/components/Games/GameContainer.tsx
+++ frontend/src/components/Games/GameContainer.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import TicTacToeDisplay from "./GameDisplays/TicTacToeDisplay";
+import TTLDisplay from "./GameDisplays/TTLDisplay";
+import HangmanDisplay from "./GameDisplays/HangmanDisplay";
+import {Button} from "@chakra-ui/react";
 
-export default function GameContainer(): JSX.Element {
+interface GameContainerProps {
+  gameType: string;
+  player1Username: string;
+  player2Username: string;
+}
+
+export default function GameContainer({gameType, player1Username, player2Username} : GameContainerProps): JSX.Element {
   return(
     <>
+      <div className="col-12">
+        <h1>{gameType}</h1>
+        <hr/>
+        <p>{player1Username} vs. {player2Username}</p>
+      </div>
+
+      {gameType === "TicTacToe" &&
+      <TicTacToeDisplay/>
+      }
+      {gameType === "TTL" &&
+      <TTLDisplay/>
+      }
+      {gameType === "Hangman" &&
+      <HangmanDisplay/>
+      }
+
+      <div className="row">
+        <div className="col-8">
+          <input className="form-control"
+          placeholder="Type your response here"/>
+        </div>
+        <div className="col-4">
+          <Button>
+            Submit
+          </Button>
+        </div>
+      </div>
     </>
   )
 }
