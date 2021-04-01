9d8a5d35b20a050c68755665e35fb5897cf95f84
mirandadayadkins <mirandaday16@gmail.com>
2021-04-01 17:41:29 -0400

    attached game container to menu bar for testing and set up styling for game container

--- frontend/src/components/Games/GameContainer.tsx
+++ frontend/src/components/Games/GameContainer.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {Button, Modal, ModalCloseButton, ModalContent, ModalOverlay, useDisclosure} from "@chakra-ui/react";
+import MenuItem from "@material-ui/core/MenuItem";
+import Typography from "@material-ui/core/Typography";
 import TicTacToeDisplay from "./GameDisplays/TicTacToeDisplay";
 import TTLDisplay from "./GameDisplays/TTLDisplay";
 import HangmanDisplay from "./GameDisplays/HangmanDisplay";
-import {Button} from "@chakra-ui/react";
 
 interface GameContainerProps {
   gameType: string;
@@ -11,35 +13,53 @@ interface GameContainerProps {
 }
 
 export default function GameContainer({gameType, player1Username, player2Username} : GameContainerProps): JSX.Element {
+  const {isOpen, onOpen, onClose} = useDisclosure();
+
   return(
+    // TODO: delete menu item button
     <>
+      <MenuItem data-testid='openMenuButton' onClick={() => onOpen()}>
+        <Typography variant="body1">Game Container</Typography>
+      </MenuItem>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
       <div className="col-12">
-        <h1>{gameType}</h1>
+        <h1 className="games-headline">{gameType}</h1>
+        <ModalCloseButton />
         <hr/>
-        <p>{player1Username} vs. {player2Username}</p>
+        <p className="games-subhead">{player1Username} vs. {player2Username}</p>
+        <br/>
       </div>
 
-      {gameType === "TicTacToe" &&
-      <TicTacToeDisplay/>
-      }
-      {gameType === "TTL" &&
-      <TTLDisplay/>
-      }
-      {gameType === "Hangman" &&
-      <HangmanDisplay/>
-      }
+          <div className="games-border">
+            {gameType === "TicTacToe" &&
+            <TicTacToeDisplay/>
+            }
+            {gameType === "TTL" &&
+            <TTLDisplay/>
+            }
+            {gameType === "Hangman" &&
+            <HangmanDisplay/>
+            }
+            test data here
+          </div>
+          <br/>
 
-      <div className="row">
-        <div className="col-8">
-          <input className="form-control"
-          placeholder="Type your response here"/>
-        </div>
-        <div className="col-4">
-          <Button>
+      <div className="row games-center-div">
+          <input className="form-control games-extra-padded"
+                 size={30}
+                 placeholder="Enter your response"/>
+      </div>
+          <div className="row games-center-div">
+
+          <Button className="games-extra-padded" colorScheme="green">
             Submit
           </Button>
-        </div>
-      </div>
-    </>
+          </div>
+        </ModalContent>
+    </Modal>
+      </>
   )
 }
