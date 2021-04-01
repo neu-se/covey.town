2d84f104fd37b81b092ec1d6d6335111d687ca16
mohitchandarana <chandarana.m@husky.neu.edu>
2021-04-01 03:29:02 -0400

    boilerplate for login

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-4h0zl999.us.auth0.com"
+      clientId="GOgkbuBOaolbYi8U3FqeJUijEoJAWhUN"
+      redirectUri={window.location.origin}
+    > 
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
