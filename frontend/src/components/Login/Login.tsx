12c7af3d15dce8541bce280be7f3e7869a85ed09
Tanmay Naik <tnaik96@gmail.com>
2021-04-05 22:57:48 -0400

    add header bar and pop-up menu for profile

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -3,8 +3,6 @@ import PreJoinScreens from '../VideoCall/VideoFrontend/components/PreJoinScreens
 import MediaErrorSnackbar
   from '../VideoCall/VideoFrontend/components/PreJoinScreens/MediaErrorSnackbar/MediaErrorSnackbar';
 import { TownJoinResponse } from '../../classes/TownsServiceClient';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
 import UserDetails from "../VideoCall/VideoFrontend/components/UserDetails/UserDetails";
 
 interface LoginProps {
@@ -17,8 +15,6 @@ export default function Login({ doLogin }: LoginProps): JSX.Element {
   return (
     <>
       <MediaErrorSnackbar error={mediaError} dismissError={() => setMediaError(undefined)} />
-      <LoginButton/>
-      <LogoutButton/>
       <UserDetails/>
       <PreJoinScreens
         doLogin={doLogin}
--- frontend/src/components/VideoCall/VideoFrontend/components/UserDetails/UserDetails.tsx
+++ frontend/src/components/VideoCall/VideoFrontend/components/UserDetails/UserDetails.tsx
@@ -1,27 +1,109 @@
-import React from 'react'
 import "./UserDetails.css";
-import { useAuth0 } from '@auth0/auth0-react';
+import { RedirectLoginOptions, useAuth0 } from '@auth0/auth0-react';
+import React, { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const NavLink = ({ children, url }: { children: ReactNode, url: (options?: RedirectLoginOptions | undefined) => Promise<void> }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded='md'
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    onClick={url}
+    href='/'>
+    {children}
+  </Link>
+);
 
 const UserDetails = () => {
-  const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, logout, loginWithRedirect } = useAuth0();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  if (isAuthenticated) {
     return (
-      <div className="userDetails">
-        <div title={user.email} className="avatar">
-          <img
-            src={user.picture}
+      <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems='center' justifyContent='space-between'>
+          <IconButton
+            size='md'
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label='Open Menu'
+            display={{ md: !isOpen ? 'none' : 'inherit' }}
+            onClick={isOpen ? onClose : onOpen}
           />
-        </div>
-        <div className="userName">
-          User: {user.name}
-        </div>
-      </div>
-    );
-  }
+          <HStack spacing={8} alignItems='center'>
+            <Box>Covey.Town</Box>
+          </HStack>
+          
+          { isAuthenticated &&
+          <Flex alignItems='center'>
+            <Menu>
+              <Box>Hello, {user.name}!</Box> 
+              <MenuButton
+                as={Button}
+                rounded='full'
+                variant='link'
+                cursor='pointer'>
+                <Avatar
+                  name = {user.name}
+                  size='sm'
+                  src={user.picture}
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>View Profile</MenuItem>
+                <MenuDivider />
+                <MenuItem onClick={() => logout({ returnTo: window.location.origin })}>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+          }
 
-  return (<div className="userName">Please log in before using covey town</div>);
+          { !isAuthenticated &&
+          <Flex alignItems='center'>
+            <Menu>
+            <HStack
+              as='nav'
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+                <NavLink url={loginWithRedirect}>Login</NavLink>              
+            </HStack>
+            </Menu>
+          </Flex>
+          }
 
+        {/* {isOpen ? (
+          <Box pb={4}>
+            <Stack as='nav' spacing={4}>
+              <NavLink url={loginWithRedirect}>Login</NavLink>
+            </Stack>
+          </Box>
+        ) : null} */}
+
+        </Flex>
+
+      </Box>
+      </>
+      );
 };
 
 export default UserDetails;
