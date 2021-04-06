feea85841d5f519192e4baa137f94a5cccc7d05d
mirandadayadkins <mirandaday16@gmail.com>
2021-04-06 19:37:08 -0400

    connected hangman game to backend move() and alreadyguessed

--- frontend/src/components/Games/GameDisplays/Hangman/HangmanDisplay.tsx
+++ frontend/src/components/Games/GameDisplays/Hangman/HangmanDisplay.tsx
@@ -1,49 +1,57 @@
 import React from 'react'
 import HangmanLetter from "./HangmanLetter";
 import HangmanGame from "../../../../../../services/roomService/src/games/HangmanGame";
+
 interface HangmanDisplayProps {
   game: HangmanGame
 }
 
 export default function HangmanDisplay({game} : HangmanDisplayProps): JSX.Element {
+  // TODO: how to refresh alreadyGuessedLetters after each click? Need move() listener?
+  const alreadyGuessedLetters = game.alreadyGuessed.sort()
   return(
     <>
+    <div className="games-border">
+      {alreadyGuessedLetters.map(letter =>
+      <span key={letter}>{letter}</span>
+      )}
+    </div>
       {/* TODO: Hangman graphics go here */}
       <br/>
       <div className="games-center-div">
         <div className="row">
-        <HangmanLetter letter="A"/>
-        <HangmanLetter letter="B"/>
-        <HangmanLetter letter="C"/>
-        <HangmanLetter letter="D"/>
-        <HangmanLetter letter="E"/>
-        <HangmanLetter letter="F"/>
-        <HangmanLetter letter="G"/>
+        <HangmanLetter game={game} letter="A"/>
+        <HangmanLetter game={game} letter="B"/>
+        <HangmanLetter game={game} letter="C"/>
+        <HangmanLetter game={game} letter="D"/>
+        <HangmanLetter game={game} letter="E"/>
+        <HangmanLetter game={game} letter="F"/>
+        <HangmanLetter game={game} letter="G"/>
       </div>
       <div className="row">
-        <HangmanLetter letter="H"/>
-        <HangmanLetter letter="I"/>
-        <HangmanLetter letter="J"/>
-        <HangmanLetter letter="K"/>
-        <HangmanLetter letter="L"/>
-        <HangmanLetter letter="M"/>
-        <HangmanLetter letter="N"/>
+        <HangmanLetter game={game} letter="H"/>
+        <HangmanLetter game={game} letter="I"/>
+        <HangmanLetter game={game} letter="J"/>
+        <HangmanLetter game={game} letter="K"/>
+        <HangmanLetter game={game} letter="L"/>
+        <HangmanLetter game={game} letter="M"/>
+        <HangmanLetter game={game} letter="N"/>
       </div>
       <div className="row">
-        <HangmanLetter letter="O"/>
-        <HangmanLetter letter="P"/>
-        <HangmanLetter letter="Q"/>
-        <HangmanLetter letter="R"/>
-        <HangmanLetter letter="S"/>
-        <HangmanLetter letter="T"/>
+        <HangmanLetter game={game} letter="O"/>
+        <HangmanLetter game={game} letter="P"/>
+        <HangmanLetter game={game} letter="Q"/>
+        <HangmanLetter game={game} letter="R"/>
+        <HangmanLetter game={game} letter="S"/>
+        <HangmanLetter game={game} letter="T"/>
       </div>
       <div className="row">
-        <HangmanLetter letter="U"/>
-        <HangmanLetter letter="V"/>
-        <HangmanLetter letter="W"/>
-        <HangmanLetter letter="X"/>
-        <HangmanLetter letter="Y"/>
-        <HangmanLetter letter="Z"/>
+        <HangmanLetter game={game} letter="U"/>
+        <HangmanLetter game={game} letter="V"/>
+        <HangmanLetter game={game} letter="W"/>
+        <HangmanLetter game={game} letter="X"/>
+        <HangmanLetter game={game} letter="Y"/>
+        <HangmanLetter game={game} letter="Z"/>
       </div>
       </div>
       </>
--- frontend/src/components/Games/GameDisplays/Hangman/HangmanLetter.tsx
+++ frontend/src/components/Games/GameDisplays/Hangman/HangmanLetter.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {Button} from "@chakra-ui/react";
-import HangmanGame from "../../gamesService/HangmanGame";
+import HangmanGame from "../../../../../../services/roomService/src/games/HangmanGame";
 
 interface HangmanLetterProps {
   game: HangmanGame;
