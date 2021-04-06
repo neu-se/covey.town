d38d44f0f33e660722f9d6ee1988b6865d581d69
commodore98 <80706639+commodore98@users.noreply.github.com>
2021-04-06 10:03:45 -0400

    centered things

--- frontend/src/components/Popup/Popup.tsx
+++ frontend/src/components/Popup/Popup.tsx
@@ -17,6 +17,7 @@ import {
     Container,
     Image,
     Box,
+    extendTheme,
   } from "@chakra-ui/react";
 import React, { useEffect, useState } from 'react';
 import useNearbyPlayers from '../../hooks/useNearbyPlayers';
@@ -27,10 +28,30 @@ import * as board from "./checker-board.png";
 //     showing : boolean;
 // }
 
+
 export default function Popup(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { nearbyPlayers } = useNearbyPlayers();
   const hasNearbyPlayer = nearbyPlayers.length > 0;
+
+  // 2. Define the new text styles
+  const theme = extendTheme({
+    textStyles: {
+      h1: {
+        // you can also use responsive styles
+        fontSize: ["72px", "98px"],
+        fontWeight: "bold",
+        lineHeight: "110%",
+        letterSpacing: "-2%",
+      },
+      h2: {
+        fontSize: ["36px", "48px"],
+        fontWeight: "semibold",
+        lineHeight: "110%",
+        letterSpacing: "-1%",
+      },
+    },
+  })
 
   return (
     <>
@@ -39,17 +60,15 @@ export default function Popup(): JSX.Element {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader ml={150} textStyle="h1">Checkers</ModalHeader>
           <ModalCloseButton />
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button colorScheme="blue" mr={100} onClick={onClose}>
               Close
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
+            <Button mr={40} variant="ghost">Play</Button>
           </ModalFooter>
-          <Box boxSize="sm">
-            <Image src="./checker-board.png" alt="Checkerboard" />
-          </Box>
+            <Image ml={75} mb={20} boxSize="300px" src="./checker-board.png" alt="Checkerboard" />
         </ModalContent>
       </Modal>
     </>
