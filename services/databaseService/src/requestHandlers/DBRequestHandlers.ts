5f3b1e5fcf98b6b71b8b83b05e8b78dc8974877d
Yashwanth Akula <akulayash1996@gmail.com>
2021-04-08 14:27:48 -0400

    wip

--- services/databaseService/src/requestHandlers/DBRequestHandlers.ts
+++ services/databaseService/src/requestHandlers/DBRequestHandlers.ts
@@ -1,20 +1,20 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-const DB_NAME = 'coveyTown';
-  
-const  COLLECTION_NAME = 'users';
+const DB_NAME = "coveyTown";
+
+const COLLECTION_NAME = "users";
 
 export interface User {
-  firstName: string,
-  lastName: string,
-  email: string,
-  friends: string [],
-  isOnline: boolean
+  firstName: string;
+  lastName: string;
+  email: string;
+  friends: string[];
+  isOnline: boolean;
 }
-  
+
 export interface UserStatus {
-  email: string,
-  isOnline: boolean
+  email: string;
+  isOnline: boolean;
 }
 
 /**
@@ -26,33 +26,34 @@ export interface ResponseEnvelope<T> {
   response?: T;
 }
 
-export interface AddUserRequest{
-  user: User,
+export interface AddUserRequest {
+  user: User;
 }
 
 export interface UserEmailRequest {
-  email: string,
+  email: string;
 }
 
 export interface StatusChangeRequest {
-  email: string,
-  status: boolean,
+  email: string;
+  status: boolean;
 }
 
-export interface AddFriendRequest{
-  email: string,
-  friendEmail: string,
+export interface AddFriendRequest {
+  email: string;
+  friendEmail: string;
 }
 
 export interface RemoveFriendRequest {
-  email: string, 
-  friendEmail: string,
+  email: string;
+  friendEmail: string;
 }
 
-export default class MongoClientFactory{
-  private uri = 'mongodb+srv://dbUser:dbUserPassword@cluster0.rdokz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-  
-  private static _instance:MongoClientFactory;
+export default class MongoClientFactory {
+  private uri =
+    "mongodb+srv://dbUser:dbUserPassword@cluster0.rdokz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+  private static _instance: MongoClientFactory;
 
   static getInstance(): MongoClientFactory {
     if (MongoClientFactory._instance === undefined) {
@@ -61,16 +62,24 @@ export default class MongoClientFactory{
     return MongoClientFactory._instance;
   }
 
-  public getMongoClient():MongoClient{
-    const client:MongoClient = new MongoClient(this.uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect();
+  public async getMongoClient(): Promise<MongoClient> {
+    const client: MongoClient = new MongoClient(this.uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
     return client;
   }
 }
 
-export async function userExistsHandler(requestData: UserEmailRequest):Promise<ResponseEnvelope<boolean>> {
-  const client:MongoClient = MongoClientFactory.getInstance().getMongoClient();
-  const userEmails =  await client.db(DB_NAME).collection(COLLECTION_NAME).distinct('email');
+export async function userExistsHandler(
+  requestData: UserEmailRequest
+): Promise<ResponseEnvelope<boolean>> {
+  const client: MongoClient = await MongoClientFactory.getInstance().getMongoClient();
+  const userEmails = await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .distinct("email");
   client.close();
   const result = requestData.email in userEmails;
   return {
@@ -79,11 +88,21 @@ export async function userExistsHandler(requestData: UserEmailRequest):Promise<R
   };
 }
 
-export async function getFriendsHandler( requestData: UserEmailRequest):Promise<ResponseEnvelope<UserStatus[]>>{
-  const client:MongoClient = MongoClientFactory.getInstance().getMongoClient();
-  const user = await client.db(DB_NAME).collection(COLLECTION_NAME).findOne({email:requestData.email});
-  const {friends} = user;
-  const friendStatuses:UserStatus[] = await client.db(DB_NAME).collection(COLLECTION_NAME).find( {email: { $in:friends }}).project({ email: 1, isOnline: 1, _id:0} ).toArray();
+export async function getFriendsHandler(
+  requestData: UserEmailRequest
+): Promise<ResponseEnvelope<UserStatus[]>> {
+  const client: MongoClient = await MongoClientFactory.getInstance().getMongoClient();
+  const user = await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .findOne({ email: requestData.email });
+  const { friends } = user;
+  const friendStatuses: UserStatus[] = await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .find({ email: { $in: friends } })
+    .project({ email: 1, isOnline: 1, _id: 0 })
+    .toArray();
   client.close();
   return {
     isOK: true,
@@ -91,64 +110,117 @@ export async function getFriendsHandler( requestData: UserEmailRequest):Promise<
   };
 }
 
-export async function getStatusHandler( requestData: UserEmailRequest):Promise<ResponseEnvelope<boolean>>{
-  const client:MongoClient = MongoClientFactory.getInstance().getMongoClient();
-  const user = await client.db(DB_NAME).collection(COLLECTION_NAME).findOne({email: requestData.email});
+export async function getAllUsersHandler(): Promise<
+  ResponseEnvelope<string[]>
+> {
+  const client: MongoClient = await MongoClientFactory.getInstance().getMongoClient();
+  const userEmails = await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .distinct("email");
+
   return {
     isOK: true,
-    response:user.isOnline,
+    response: userEmails,
+  };
+}
+export async function getStatusHandler(
+  requestData: UserEmailRequest
+): Promise<ResponseEnvelope<boolean>> {
+  const client: MongoClient = await MongoClientFactory.getInstance().getMongoClient();
+  const user = await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .findOne({ email: requestData.email });
+  return {
+    isOK: true,
+    response: user.isOnline,
   };
 }
 
-export async function setStatusHandler(requestData: StatusChangeRequest):Promise<void>{
-  const client:MongoClient = MongoClientFactory.getInstance().getMongoClient();
-  await client.db(DB_NAME).collection(COLLECTION_NAME).updateOne({email: requestData.email}, { $set: {isOnline: requestData.status}});
+export async function setStatusHandler(
+  requestData: StatusChangeRequest
+): Promise<void> {
+  const client: MongoClient = await MongoClientFactory.getInstance().getMongoClient();
+  await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .updateOne(
+      { email: requestData.email },
+      { $set: { isOnline: requestData.status } }
+    );
   client.close();
 }
 
-export async function addFriendHandler(requestData: AddFriendRequest):Promise<ResponseEnvelope<Record<string, null>>>{
-  const client:MongoClient = MongoClientFactory.getInstance().getMongoClient();
-  const user = await client.db(DB_NAME).collection(COLLECTION_NAME).findOne({email: requestData.email});
-  const {friends} = user;
+export async function addFriendHandler(
+  requestData: AddFriendRequest
+): Promise<ResponseEnvelope<Record<string, null>>> {
+  const client: MongoClient = await MongoClientFactory.getInstance().getMongoClient();
+  const user = await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .findOne({ email: requestData.email });
+  const { friends } = user;
   // check if the person with this email-id exists in the database
-  const userEmails =  await client.db(DB_NAME).collection(COLLECTION_NAME).distinct('email');
+  const userEmails = await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .distinct("email");
   const friendExists = requestData.friendEmail in userEmails;
 
   // check if the friend has already been added
-  const shouldInsert = !friends.includes(requestData.friendEmail) && friendExists && (requestData.friendEmail !== requestData.email);
-  if (shouldInsert){
-    await client.db(DB_NAME).collection(COLLECTION_NAME).updateOne({email: requestData.email}, { $push: {friends: requestData.friendEmail}});
+  const shouldInsert =
+    !friends.includes(requestData.friendEmail) &&
+    friendExists &&
+    requestData.friendEmail !== requestData.email;
+  if (shouldInsert) {
+    await client
+      .db(DB_NAME)
+      .collection(COLLECTION_NAME)
+      .updateOne(
+        { email: requestData.email },
+        { $push: { friends: requestData.friendEmail } }
+      );
     client.close();
     return {
       isOK: true,
       response: {},
-      message: 'friend added to your list',
+      message: "friend added to your list",
     };
-  } 
+  }
   client.close();
   return {
     isOK: false,
     response: {},
-    message: 'friend not added: either they are not in the database or they are already in your lists.',
+    message:
+      "friend not added: either they are not in the database or they are already in your lists.",
   };
 }
 
-export async function addUserHandler(requestData: AddUserRequest):Promise<ResponseEnvelope<Record<string, null>>>{
-  const client:MongoClient = MongoClientFactory.getInstance().getMongoClient();
-  const userEmails =  await client.db(DB_NAME).collection(COLLECTION_NAME).distinct('email');
-  const shouldInsert =  !(requestData.user.email in userEmails);
-  if (shouldInsert){
-    await client.db(DB_NAME).collection(COLLECTION_NAME).insertOne(requestData.user);
+export async function addUserHandler(
+  requestData: AddUserRequest
+): Promise<ResponseEnvelope<Record<string, null>>> {
+  const client: MongoClient = await MongoClientFactory.getInstance().getMongoClient();
+  const userEmails = await client
+    .db(DB_NAME)
+    .collection(COLLECTION_NAME)
+    .distinct("email");
+  const shouldInsert = !(requestData.user.email in userEmails);
+  if (shouldInsert) {
+    await client
+      .db(DB_NAME)
+      .collection(COLLECTION_NAME)
+      .insertOne(requestData.user);
     client.close();
     return {
       isOK: true,
-      message: 'Added user',
+      message: "Added user",
     };
-  } 
+  }
   client.close();
   return {
     isOK: false,
-    message: 'user was not added',
+    message: "user was not added",
   };
 }
 
--- services/databaseService/src/router/database.ts
+++ services/databaseService/src/router/database.ts
@@ -1,95 +1,119 @@
-import { Express } from 'express';
-import BodyParser from 'body-parser';
-import io from 'socket.io';
-import { Server } from 'http';
-import { StatusCodes } from 'http-status-codes';
-import { userExistsHandler, getFriendsHandler, addFriendHandler, getStatusHandler, setStatusHandler, addUserHandler } from '../requestHandlers/DBRequestHandlers';
-import { logError } from '../../../roomService/src/Utils';
+import BodyParser from "body-parser";
+import { Express } from "express";
+import { Server } from "http";
+import { StatusCodes } from "http-status-codes";
+import io from "socket.io";
+import {
+  addFriendHandler,
+  addUserHandler,
+  getAllUsersHandler,
+  getFriendsHandler,
+  getStatusHandler,
+  setStatusHandler,
+  userExistsHandler,
+} from "../requestHandlers/DBRequestHandlers";
+import { logError } from "../Utils";
 
-export default function addDBRoutes(http: Server, app: Express):io.Server {
-
-  app.get('/users?email=emailID', BodyParser.json(), async (req, res) => {
+export default function addDBRoutes(http: Server, app: Express): io.Server {
+  app.get("/users?email=emailID", BodyParser.json(), async (req, res) => {
     try {
-      const result = userExistsHandler({email: req.params.email});
-      res.status(StatusCodes.OK)
-        .json(result);
-    } catch (err) {
-      logError(err);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({
-          message: 'Internal server error, please see log in server for more details',
-        });
-    }
-  });
-
-  app.get('/users/:emailID/friends', BodyParser.json(), async (req, res) => {
-    try {
-      const result = getFriendsHandler({email: req.params.emailID});
+      const result = await userExistsHandler({ email: req.params.email });
       res.status(StatusCodes.OK).json(result);
     } catch (err) {
       logError(err);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({
-          message: 'Internal server error, please see log in server for more details',
-        });
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message:
+          "Internal server error, please see log in server for more details",
+      });
     }
   });
 
-  app.get('/users/:emailID/status', BodyParser.json(), async (req, res) => {
+  app.get("/users", BodyParser.json(), async (_req, res) => {
     try {
-      const result = getStatusHandler({email: req.params.emailID});
+      const result = await getAllUsersHandler();
+      console.log("result:" + result);
       res.status(StatusCodes.OK).json(result);
     } catch (err) {
       logError(err);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({
-          message: 'Internal server error, please see log in server for more details',
-        });
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message:
+          "Internal server error, please see log in server for more details",
+      });
     }
   });
 
-  app.post('/users/:emailID/status/', BodyParser.json(), async (req, res) => {
+  app.get("/users/:emailID/friends", BodyParser.json(), async (req, res) => {
+    try {
+      const result = await getFriendsHandler({ email: req.params.emailID });
+      res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+      logError(err);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message:
+          "Internal server error, please see log in server for more details",
+      });
+    }
+  });
+
+  app.get("/users/:emailID/status", BodyParser.json(), async (req, res) => {
+    try {
+      const result = await getStatusHandler({ email: req.params.emailID });
+      res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+      logError(err);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message:
+          "Internal server error, please see log in server for more details",
+      });
+    }
+  });
+
+  app.post("/users/:emailID/status/", BodyParser.json(), async (req, res) => {
     try {
       const result = await setStatusHandler(req.body.status);
-      res.status(StatusCodes.OK)
-        .json(result);
+      res.status(StatusCodes.OK).json(result);
     } catch (err) {
       logError(err);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({
-          message: 'Internal server error, please see log in server for more details',
-        });
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message:
+          "Internal server error, please see log in server for more details",
+      });
     }
   });
 
-  app.post('/users', BodyParser.json(), async (req, res) => {
+  app.post("/users", BodyParser.json(), async (req, res) => {
     try {
       const result = await addUserHandler(req.body.status);
-      res.status(StatusCodes.OK)
-        .json(result);
+      res.status(StatusCodes.OK).json(result);
     } catch (err) {
       logError(err);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({
-          message: 'Internal server error, please see log in server for more details',
-        });
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message:
+          "Internal server error, please see log in server for more details",
+      });
     }
   });
 
-  app.post('/users/:emailID/friends/:friendEmailID ', BodyParser.json(), async (req, res) => {
-    try {
-      const result = await addFriendHandler({email: req.params.emailID, friendEmail: req.params.friendEmailID});
-      res.status(StatusCodes.OK)
-        .json(result);
-    } catch (err) {
-      logError(err);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({
-          message: 'Internal server error, please see log in server for more details',
+  app.post(
+    "/users/:emailID/friends/:friendEmailID ",
+    BodyParser.json(),
+    async (req, res) => {
+      try {
+        const result = await addFriendHandler({
+          email: req.params.emailID,
+          friendEmail: req.params.friendEmailID,
         });
+        res.status(StatusCodes.OK).json(result);
+      } catch (err) {
+        logError(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          message:
+            "Internal server error, please see log in server for more details",
+        });
+      }
     }
-  });
+  );
 
-  const socketServer = new io.Server(http, { cors: { origin: '*' } });
+  const socketServer = new io.Server(http, { cors: { origin: "*" } });
   return socketServer;
 }
