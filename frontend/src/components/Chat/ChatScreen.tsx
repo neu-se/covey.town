52110af6c9361f48231546d33dfda9f29a378e50
harishsurf <harishofficial92@gmail.com>
2021-04-09 14:41:08 -0400

    Added Dropdown component

--- frontend/src/components/Chat/ChatScreen.tsx
+++ frontend/src/components/Chat/ChatScreen.tsx
@@ -1,26 +1,47 @@
 import React, { Component, useState } from "react";
+import { Select } from '@chakra-ui/react';
+import useCoveyAppState from "../../hooks/useCoveyAppState";
 import './ChatScreen.css';
 
+
 const ChatScreen = () => {
-  const [count, setCount] = useState(0);
+  const [inputMsg, setInputMsg] = useState('');
 
-  const  increment = () => {
-        setCount(count + 1);
-    };
+  const [receiver, setReceiver] = useState('everyone');
 
-    return (
-      <div>
-          <body>
-            <div className='heading'>Chat Box</div>
-            <div className='mbox'>
-                <ul id="messages"/>
-            </div>
-            <form id="form" action="">
-              <input id="input" autoComplete="off" />
-              <button type='submit' >Send</button>
-            </form>
-          </body>
-      </div>
-    );
+  const {
+    players, myPlayerID, socket
+  } = useCoveyAppState();
+
+  console.log("myPlayerID", myPlayerID);
+
+  const handleSubmit = () =>  {
+    if(receiver ===  'everyone') {
+        // send to all
+    } else {
+      // send to receiver player id 
+    }
+    // reset message to empty once sent
+    setInputMsg(''); 
+
+  }
+
+  return (
+    <div>
+      <body>
+        <div className='heading'>Chat Box</div>
+        <div className='mbox'>
+          <Select onChange={(e) => setReceiver(e.target.value)}>
+            <option value="everyone">Everyone</option>
+            {players.filter(player => player.id !== myPlayerID).map(player => <option key={player.userName}  value={player.id}> {player.userName} </option>)}
+          </Select>
+        </div>
+        <form id="form" onSubmit={()=> handleSubmit()}>
+          <input id="input" autoComplete="off" onChange={(e) => setInputMsg(e.target.value)} />
+          <button type='submit'>Send</button>
+        </form>
+      </body>
+    </div>
+  );
 };
 export default ChatScreen
