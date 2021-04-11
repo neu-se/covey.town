a4a2e22f0c8095d640a0550f43df7f1d64f7c6f9
Maddy Van Hulse <hulse.madeline@gmail.com>
2021-04-11 15:33:00 -0400

    add checkers to board and make draggable

--- frontend/src/components/world/ReactCheckers.tsx
+++ frontend/src/components/world/ReactCheckers.tsx
@@ -6,6 +6,7 @@ import React, { useContext, useEffect, useState } from 'react';
  import useNearbyPlayers from '../../hooks/useNearbyPlayers';
 
  class ReactCheckersScene extends Phaser.Scene {
+        
      constructor(){
          super({
              key: "checkers"
@@ -31,13 +32,6 @@ import React, { useContext, useEffect, useState } from 'react';
      }
 
      create() {
-
-         this.setUpPopupUI();
-
-     }
-
-     setUpPopupUI() {
-
          // Create container
          const windowContainer = this.add.container(this.game.renderer.width * 0.2, this.game.renderer.height * 0.1);
 
@@ -48,7 +42,11 @@ import React, { useContext, useEffect, useState } from 'react';
              .setOrigin(0);
          windowContainer.add([backgroundImage, playButton]);
 
-         // this.addCheckersToBoard();
+         this.input.on('drag', (pointer: Phaser.Input.Pointer, gameObject: any, dragX: number, dragY: number) => {
+             const object = gameObject;
+             object.x = dragX;
+             object.y = dragY;
+        });
      }
 
      addPlayButton(x :number, y :number, container :Phaser.GameObjects.Container) {
@@ -73,26 +71,74 @@ import React, { useContext, useEffect, useState } from 'react';
      }
 
      startGame(container :Phaser.GameObjects.Container) {
-         container.removeAll(true);
-         const gameboardImage = this.add.image(0, 0, 'checkerBoardWithBackground', 0)
-             .setScale(0.75, 0.75)
-             .setOrigin(0);
-             container.add(gameboardImage);
+        
+        // Remove popup items from container
+        container.removeAll(true);
+         
+        // Add gameboard background
+        const gameboardImage = this.add.image(0, 0, 'checkerBoardWithBackground', 0).setScale(0.75, 0.75).setOrigin(0);
+        this.addCheckersToBoard(container);
+
+        container.add(gameboardImage);
+        container.sendToBack(gameboardImage);
      }
 
-     addCheckersToBoard() {
-         this.addCheckersToBoard = () => {
-             // Rows
-             for (let r = 0; r <= 2; r += 1) {
-                 // Each checker
-                 for (let i = 0; i <= 7; i += 1) {
-                     // const checker = new Checker(this);
-                     // checker.render(, '')
-                 }
-             }
-         }
-         console.log('added checkers to board');
-     }
+     addCheckersToBoard(container: Phaser.GameObjects.Container) {
+        // Iterate through rows of the board
+        for (let r = 0; r <= 7; r += 1) {
+            switch (r) {
+                case 0:
+                    this.addCheckerRow(r, 'redChecker', 1, container);
+                    break;
+                case 1:
+                    this.addCheckerRow(r, 'redChecker', 2, container);
+                    break;
+                case 2:
+                    this.addCheckerRow(r, 'redChecker', 1, container);
+                    break;
+                case 3:
+                    // (no checkers)
+                    break;
+                case 4:
+                    // (no checkers)
+                    break;
+                case 5:
+                    this.addCheckerRow(r, 'blackChecker', 2, container);
+                    break;
+                case 6:
+                    this.addCheckerRow(r, 'blackChecker', 1, container);
+                    break;
+                case 7:
+                    this.addCheckerRow(r, 'blackChecker', 2, container);
+                    break;
+                default: 
+                    break;
+            }
+        }    
+        console.log('added checkers to board');
+    }
+
+    addCheckerRow(row: number, color: string, type: number, container: Phaser.GameObjects.Container) {
+        if (type === 1) {
+            for (let i = 0; i <= 7; i += 1) {
+                if (i % 2 === 0) {
+                    const checker = this.add.image((i * 60) + 90, (row * 60) + 90, color).setScale(0.75, 0.75).setInteractive();
+                    this.input.setDraggable(checker);
+                    container.add(checker);
+                    console.log('added red checker');
+                }
+            }
+        } else {
+            for (let i = 0; i <= 7; i += 1) {
+                if (i % 2 === 1) {
+                    const checker = this.add.image((i * 60) + 90, (row * 60) + 90, color).setScale(0.75, 0.75).setInteractive();
+                    this.input.setDraggable(checker);
+                    container.add(checker);
+                    console.log('added black checker');
+                }
+            }
+        }
+    }
  }
 
  export default function ReactCheckers(): JSX.Element {
