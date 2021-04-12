56e347f804da407c2a0a3437501e4727952fd20f
Shaun <shaun.khundker@gmail.com>
2021-04-12 00:36:00 -0400

    front end chat design changes
    changed skeleton for chat in the front end to look better. the chat box is not flexible yet and still need to implement functionality

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,18 +1,55 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
+import { Box, Button, Flex, Input } from '@chakra-ui/react';
 import React from 'react';
+import ChatMessage from './ChatMessage';
 
 export default function Chat(): JSX.Element {
   return (
-    <Flex>
-      <Box maxW='sm' m='auto' p='2' w='100%' borderWidth='2px' borderRadius='lg' overflow='hidden'>
-        <Box p='2'>Chat</Box>
-        <Box maxW='sm' m='auto' borderWidth='1px' borderRadius='lg' w='100%' p='4'>
-          user1: hi user2: hello
+    <Flex minH='768px' maxH='768px' w='100%' px='2' pt='2'>
+      <Box
+        m='auto'
+        p='2'
+        w='100%'
+        borderWidth='2px'
+        borderRadius='lg'
+        overflow='hidden'
+        minH='760px'
+        maxH='760px'
+        position='relative'
+        flexDirection = "column">
+        <Box p='2' align='left'>
+          Chat
         </Box>
-        <Box p='2' align = 'center'>
-          <Button bg = "tomato">Send global</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <Button bg = "tomato">Send private</Button>
+        <Box
+          m='auto'
+          borderWidth='1px'
+          borderRadius='lg'
+          w='100%'
+          height = '600px'
+          p='4'
+          align='left'
+          overflow = 'auto'>
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+          <ChatMessage />
+        </Box>
+        <Box w='100%' position='absolute' bottom='0' pb = '1'>
+          <Box m='auto' w='100%' pb='2' pr = "4" align='left'>
+            <Input borderColor='#4299E1' placeholder='Enter message here' />
+          </Box>
+          <Box pb='1' alignItems='center'>
+            <Button colorScheme="blue">Send global</Button>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Button colorScheme="blue">Send private</Button>
+          </Box>
         </Box>
       </Box>
     </Flex>
---
+++ frontend/src/components/Chat/ChatMessage.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+
+// import messages from TownServiceClient
+
+export default function ChatMessage(): JSX.Element {
+    return (
+          <Text>
+            user1: hi
+          </Text>
+    );
+  }
