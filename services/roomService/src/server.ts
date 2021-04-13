925a467cac1276f2ccc97395d596fcdd96612a09
Tran <pham.tr@northeastern.edu>
2021-04-13 01:40:00 -0400

    Revert "working api & database"
    This reverts commit 997f62e399fd45833e36f4bc73ed63e8b4c7d593.

--- services/roomService/src/server.ts
+++ services/roomService/src/server.ts
@@ -1,34 +1,12 @@
-import CORS from 'cors';
 import Express from 'express';
 import * as http from 'http';
-import mongoose from 'mongoose';
+import CORS from 'cors';
 import { AddressInfo } from 'net';
-import CoveyTownsStore from './lib/CoveyTownsStore';
 import addTownRoutes from './router/towns';
-import users from './router/users';
+import CoveyTownsStore from './lib/CoveyTownsStore';
 
 const app = Express();
 app.use(CORS());
-app.use(Express.json());
-app.use(Express.urlencoded({ extended: true }));
-
-users(app);
-
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${encodeURIComponent(
-  process.env.MONGO_PASSWORD || '',
-)}@coveytownusers.w8gis.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-
-const options = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-
-mongoose
-  .connect(uri, options)
-  .then(() => {
-    console.log('Connected to DB');
-  })
-  .catch(error => {
-    throw error;
-  });
-
 const server = http.createServer(app);
 
 addTownRoutes(server, app);
@@ -38,6 +16,7 @@ server.listen(process.env.PORT || 8081, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening on ${address.port}`);
   if (process.env.DEMO_TOWN_ID) {
-    CoveyTownsStore.getInstance().createTown(process.env.DEMO_TOWN_ID, false);
+    CoveyTownsStore.getInstance()
+      .createTown(process.env.DEMO_TOWN_ID, false);
   }
 });
--- services/roomService/src/types/UserAccount.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface IUserAccount extends Document {
-  username: string;
-  password: string;
-  avatar: string;
-}
