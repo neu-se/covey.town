beabc32046917b04038f43a35978b8722a508afa
mirandadayadkins <mirandaday16@gmail.com>
2021-04-14 21:09:09 -0400

    logic for hangman display added

--- frontend/src/components/Games/GameDisplays/Hangman/HangmanFigure.tsx
+++ frontend/src/components/Games/GameDisplays/Hangman/HangmanFigure.tsx
@@ -8,7 +8,34 @@ export default function HangmanDisplay({game}: HangmanFigureProps): JSX.Element 
   const imageBasePath : string = "../../../../../public/assets/"
   return (
     <>
-      <img src={`${imageBasePath}`} alt=""/>
+      {
+        game.limbList.length === 6 &&
+        <img src={`${imageBasePath}noLimbs.jpg`} alt="gallowsOnly"/>
+      }
+      {
+        game.limbList.length === 5 &&
+        <img src={`${imageBasePath}head.jpg`} alt="headAdded"/>
+      }
+      {
+        game.limbList.length === 4 &&
+        <img src={`${imageBasePath}back.jpg`} alt="backAdded"/>
+      }
+      {
+        game.limbList.length === 3 &&
+        <img src={`${imageBasePath}oneArm.jpg`} alt="oneArmAdded"/>
+      }
+      {
+        game.limbList.length === 2 &&
+        <img src={`${imageBasePath}twArms.jpg`} alt="twoArmsAdded"/>
+      }
+      {
+        game.limbList.length === 1 &&
+        <img src={`${imageBasePath}oneLeg.jpg`} alt="oneLegAdded"/>
+      }
+      {
+        game.limbList.length === 6 &&
+        <img src={`${imageBasePath}twoLegs.jpg`} alt="twoLegsAdded"/>
+      }
     </>
   )
 }
