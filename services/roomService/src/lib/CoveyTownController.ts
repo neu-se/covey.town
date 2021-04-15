09359eacc42ea47f210378f995d3e52a9a07d2b0
Julia <jnsteff22@gmail.com>
2021-04-14 22:30:11 -0400

    Got emojify import to work, added emojify tests

--- services/roomService/src/lib/CoveyTownController.ts
+++ services/roomService/src/lib/CoveyTownController.ts
@@ -9,7 +9,7 @@ import PlayerSession from '../types/PlayerSession';
 import PrivateChatMessage from '../types/PrivateChatMessage';
 import IVideoClient from './IVideoClient';
 import TwilioVideo from './TwilioVideo';
-// import emojify from 'emojify.js';
+import { emojify } from 'node-emoji';
 
 const friendlyNanoID = customAlphabet('1234567890ABCDEF', 8);
 
@@ -171,7 +171,7 @@ export default class CoveyTownController {
   sendPrivatePlayerMessage(message: PrivateChatMessage): void {
     const messageCensored = message;
     messageCensored.message = this._censorer.censorMessage(messageCensored.message);
-    // messageCensored = emojify.replace(message.message);
+    messageCensored.message = emojify(message.message);
     this.messages.push(messageCensored);
     this._listeners.forEach(listener => listener.onPrivateMessage(messageCensored));
   }
@@ -179,7 +179,7 @@ export default class CoveyTownController {
   sendGlobalPlayerMessage(message: GlobalChatMessage): void {
     const messageCensored = message;
     messageCensored.message = this._censorer.censorMessage(messageCensored.message);
-    // messageCensored = emojify.replace(message.message);
+    messageCensored.message = emojify(message.message);
     this.messages.push(messageCensored);
     this._listeners.forEach(listener => listener.onGlobalMessage(messageCensored));
   }
