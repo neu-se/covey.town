9871611597fcf72960f01b5118072982d7bd51bb
dhingratrena <dhingra.tr@husky.neu.edu>
2021-04-15 13:36:37 -0400

    signUp user

--- services/roomService/src/client/CoveyProfile.test.ts
+++ services/roomService/src/client/CoveyProfile.test.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import resolvers from '../resolvers';
-
+import { userModel as User } from '../data/models/users/user.model.server';
 
 beforeAll(async () => {
   const uri =
@@ -12,9 +12,29 @@ beforeAll(async () => {
   connection();
 });
 
-describe('getAllUsers', () => {
-  it('creates a course', async () => {
-    const result = await resolvers.Query.users();
-    expect(result.length).toMatchSnapshot();
+// describe('getAllUsers', () => {
+//   it('gets a list of all users', async () => {
+//     const result = await resolvers.Query.users();
+//     expect(result.length).toMatchSnapshot();
+//   });
+// });
+
+describe('signUp', () => {
+    it('creates a user', async () => {
+        const userData = {
+            userName: "testCoveyTownUser",
+            email: "jk136487@gmail.com",
+            password: "Testuser@123"
+        }
+        const user = await resolvers.Mutation.signUp({
+            
+        }, { input: userData}
+              
+        );
+        // console.log(user.email);
+    //   const userDb = await resolvers.Query.searchUserByEmail({},{input:{email: "jk16@gmail.com"}})
+    //   expect(user).toEqual(userDb);
+        
+        
   });
 });
