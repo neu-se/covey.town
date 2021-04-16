98ae681468b9ff94813b6536c4ecc86be8746c1b
Kayla Caputo <caputo.k@husky.neu.edu>
2021-04-16 13:41:26 -0400

    revert

--- frontend/src/components/VideoCall/VideoFrontend/types.ts
+++ frontend/src/components/VideoCall/VideoFrontend/types.ts
@@ -12,7 +12,7 @@ declare module 'twilio-video' {
   }
 
   interface LocalTrackPublication {
-    setPriority: (priority: Track.Priority) => this;
+    setPriority: (priority: Track.Priority) => LocalTrackPublication;
   }
 
   interface LocalVideoTrack {
@@ -22,7 +22,7 @@ declare module 'twilio-video' {
 
   interface RemoteVideoTrack {
     isSwitchedOff: boolean;
-    setPriority: (priority: Track.Priority | null) => this;
+    setPriority: (priority: Track.Priority) => RemoteVideoTrack;
   }
 
   interface VideoBandwidthProfileOptions {
