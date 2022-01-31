9374b7d0f81671c46c3a5fca2298b3059735112e
Jonathan Bell <jon@jonbell.net>
2022-01-31 17:53:29 -0500

    Expands on issue #91 - pause/unpause game properly when new conversation area modal is displayed or removed. Also ensure that the player stops moving in case the arrow key was being held at the moment that space was pressed.

--- frontend/src/components/world/WorldMap.tsx
+++ frontend/src/components/world/WorldMap.tsx
@@ -7,8 +7,10 @@ import useConversationAreas from '../../hooks/useConversationAreas';
 import useCoveyAppState from '../../hooks/useCoveyAppState';
 import usePlayerMovement from '../../hooks/usePlayerMovement';
 import usePlayersInTown from '../../hooks/usePlayersInTown';
+import { Callback } from '../VideoCall/VideoFrontend/types';
 import NewConversationModal from './NewCoversationModal';
 
+// Original inspiration and code from:
 // https://medium.com/@michaelwesthadley/modular-game-worlds-in-phaser-3-tilemaps-1-958fc7e6bbd6
 class CoveyGameScene extends Phaser.Scene {
   private player?: {
@@ -47,7 +49,7 @@ class CoveyGameScene extends Phaser.Scene {
 
   private setNewConversation: (conv: ConversationArea) => void;
 
-  private pendingInitConversationAreas?: ConversationArea[];
+  private _onGameReadyListeners: Callback[] = [];
 
   constructor(
     video: Video,
@@ -76,33 +78,44 @@ class CoveyGameScene extends Phaser.Scene {
     this.load.atlas('atlas', '/assets/atlas/atlas.png', '/assets/atlas/atlas.json');
   }
 
-  updateConversationAreas(conversationAreas: ConversationArea[]) {
-    this.pendingInitConversationAreas = conversationAreas;
-    if (!this.ready) {
-      return;
-    }
-    this._updateConversationAreas();
+  _updateConversationAreas(_conversationAreas: ConversationArea[]) {
+    _conversationAreas.forEach(eachUpdatedArea => {
+      const existingArea = this.conversationAreas.find(
+        eachExistingArea => eachExistingArea.label === eachUpdatedArea.label,
+      );
+      if (existingArea) {
+        if (existingArea.topic !== eachUpdatedArea.topic) {
+          existingArea.onTopicChange(eachUpdatedArea.topic);
+        }
+      }
+    });
+    this.conversationAreas.forEach(eachArea => {
+      const serverArea = _conversationAreas?.find(a => a.label === eachArea.label);
+      if (!serverArea) {
+        eachArea.destroy();
+      }
+    });
   }
 
-  _updateConversationAreas() {
-    if (this.pendingInitConversationAreas) {
-      this.pendingInitConversationAreas.forEach(eachUpdatedArea => {
-        const existingArea = this.conversationAreas.find(
-          eachExistingArea => eachExistingArea.label === eachUpdatedArea.label,
-        );
-        if (existingArea) {
-          if (existingArea.topic !== eachUpdatedArea.topic) {
-            existingArea.onTopicChange(eachUpdatedArea.topic);
-          }
-        }
+  /**
+   * Update the WorldMap's view of the current conversation areas, updating their topics and
+   * participants, as necessary
+   *
+   * @param conversationAreas
+   * @returns
+   */
+  updateConversationAreas(conversationAreas: ConversationArea[]) {
+    if (!this.ready) {
+      /*
+       * Due to the asynchronous nature of setting up a Phaser game scene (it requires gathering
+       * some resources using asynchronous operations), it is possible that this could be called
+       * in the period between when the player logs in and when the game is ready. Hence, we
+       * register a callback to complete the initialization once the game is ready
+       */
+      this._onGameReadyListeners.push(() => {
+        this.updateConversationAreas(conversationAreas);
       });
-      this.conversationAreas.forEach(eachArea => {
-        const serverArea = this.pendingInitConversationAreas?.find(a => a.label === eachArea.label);
-        if (!serverArea) {
-          eachArea.destroy();
-        }
-      });
-      this.pendingInitConversationAreas = undefined;
+      
     }
   }
 
@@ -273,7 +286,6 @@ class CoveyGameScene extends Phaser.Scene {
             )
           ) {
             this.infoTextBox?.setVisible(false);
-            this.currentConversationArea.onCurrentPlayerExits();
             this.currentConversationArea = undefined;
             this.lastLocation.conversationLabel = undefined;
           }
@@ -369,10 +381,11 @@ class CoveyGameScene extends Phaser.Scene {
       );
       sprite.setTintFill();
       sprite.setAlpha(0.3);
-      const conversationAreaObj = new ConversationArea(
-        sprite.name,undefined,
-        { labelText, topicText, sprite },
-      );
+      const conversationAreaObj = new ConversationArea(sprite.name, undefined, {
+        labelText,
+        topicText,
+        sprite,
+      });
       sprite.setData('conversation', conversationAreaObj);
       this.conversationAreas.push(conversationAreaObj);
     });
@@ -483,7 +496,6 @@ class CoveyGameScene extends Phaser.Scene {
             this.infoTextBox?.setVisible(false);
           }
           this.currentConversationArea = conv;
-          conv.onCurrentPlayerEntered();
         }
       },
     );
@@ -580,24 +592,33 @@ class CoveyGameScene extends Phaser.Scene {
       // sprites....
       this.players.forEach(p => this.updatePlayerLocation(p));
     }
-    if (this.pendingInitConversationAreas) {
-      this._updateConversationAreas();
-    }
+    // Call any listeners that are waiting for the game to be initialized
+    this._onGameReadyListeners.forEach(listener => listener());
+    this._onGameReadyListeners = [];
   }
 
   pause() {
-    this.paused = true;
-    this.previouslyCapturedKeys = this.input.keyboard.getCaptures();
-    this.input.keyboard.clearCaptures();
+    if (!this.paused) {
+      this.paused = true;
+      if(this.player){
+        this.player?.sprite.anims.stop();
+        const body = this.player.sprite.body as Phaser.Physics.Arcade.Body;
+        body.setVelocity(0);
+      }
+      this.previouslyCapturedKeys = this.input.keyboard.getCaptures();
+      this.input.keyboard.clearCaptures();
+    }
   }
 
   resume() {
-    this.paused = false;
-    if (Video.instance()) {
-      // If the game is also in process of being torn down, the keyboard could be undefined
-      this.input.keyboard.addCapture(this.previouslyCapturedKeys);
+    if (this.paused) {
+      this.paused = false;
+      if (Video.instance()) {
+        // If the game is also in process of being torn down, the keyboard could be undefined
+        this.input.keyboard.addCapture(this.previouslyCapturedKeys);
+      }
+      this.previouslyCapturedKeys = [];
     }
-    this.previouslyCapturedKeys = [];
   }
 }
 
@@ -618,7 +639,7 @@ export default function WorldMap(): JSX.Element {
       pixelArt: true,
       autoRound: 10,
       minWidth: 800,
-      fps: {target: 30},
+      fps: { target: 30 },
       powerPreference: 'high-performance',
       minHeight: 600,
       physics: {
@@ -649,11 +670,11 @@ export default function WorldMap(): JSX.Element {
   useEffect(() => {
     const movementDispatcher = (player: ServerPlayer) => {
       gameScene?.updatePlayerLocation(Player.fromServerPlayer(player));
-    }
+    };
     playerMovementCallbacks.push(movementDispatcher);
     return () => {
-      playerMovementCallbacks.splice(playerMovementCallbacks.indexOf(movementDispatcher),1);
-    }
+      playerMovementCallbacks.splice(playerMovementCallbacks.indexOf(movementDispatcher), 1);
+    };
   }, [gameScene, playerMovementCallbacks]);
 
   useEffect(() => {
@@ -670,18 +691,31 @@ export default function WorldMap(): JSX.Element {
     gameScene?.updateConversationAreas(conversationAreas);
   }, [conversationAreas, deepConversationAreas, gameScene]);
 
+  const newConversationModalOpen = newConversation !== undefined;
+  useEffect(() => {
+    if (newConversationModalOpen) {
+      video?.pauseGame();
+    } else {
+      video?.unPauseGame();
+    }
+  }, [video, newConversationModalOpen]);
 
   const newConversationModal = useMemo(() => {
-    if (newConversation)
+    if (newConversation) {
+      video?.pauseGame();
       return (
         <NewConversationModal
           isOpen={newConversation !== undefined}
-          closeModal={() => setNewConversation(undefined)}
+          closeModal={() => {
+            video?.unPauseGame();
+            setNewConversation(undefined);
+          }}
           newConversation={newConversation}
         />
       );
+    }
     return <></>;
-  }, [newConversation, setNewConversation]);
+  }, [video, newConversation, setNewConversation]);
 
   return (
     <>
