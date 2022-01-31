d58796f4c64f00e816ac1c307e93575fe54579e8
Jonathan Bell <jon@jonbell.net>
2022-01-31 17:38:49 -0500

    :wave: Bye bye, expensive twilio conversation api

--- services/townService/src/lib/TwilioVideo.ts
+++ services/townService/src/lib/TwilioVideo.ts
@@ -57,31 +57,6 @@ export default class TwilioVideo implements IVideoClient {
     return TwilioVideo._instance;
   }
 
-  async createConversationIfNotExisting(conversationID: string, clientIdentity: string): Promise<void> {
-    assert(this._twilioConversationsSid);
-    if (this._twilioConversationsSid === 'IS..'){
-      // Do not try to create a conversation if we don't have a real account SID
-      return;
-    }
-    const conversationsClient = this._twilioClient.conversations.services(this._twilioConversationsSid);
-    try {
-      // Does it exist?
-      await conversationsClient.conversations(conversationID).fetch();
-    } catch (e) {
-      // Create the room
-      await conversationsClient.conversations.create({ uniqueName: conversationID });
-    }
-    // Add the participant to the conversation
-    try {
-      await conversationsClient.conversations(conversationID).participants.create({ identity: clientIdentity });
-    } catch (e) {
-      // Ignore "Participant already exists" error - we always try to add the participant, even if they are already in the conversation
-      if (e instanceof Error && e.code !== 50433){
-        throw e;
-      }
-    }
-  }
-
   async getTokenForTown(coveyTownID: string, clientIdentity: string): Promise<string> {
     const token = new Twilio.jwt.AccessToken(
       this._twilioAccountSid, this._twilioApiKeySID, this._twilioApiKeySecret, {
@@ -91,12 +66,6 @@ export default class TwilioVideo implements IVideoClient {
     token.identity = clientIdentity;
     const videoGrant = new Twilio.jwt.AccessToken.VideoGrant({ room: coveyTownID });
     token.addGrant(videoGrant);
-    if (this._twilioConversationsSid){
-      await this.createConversationIfNotExisting(coveyTownID, clientIdentity);
-      const chatGrant = new ChatGrant({ serviceSid: this._twilioConversationsSid });
-      token.addGrant(chatGrant);
-    }
-
     return token.toJwt();
   }
 }
