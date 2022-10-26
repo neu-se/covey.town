9b79b65d731a793cadf058ad18cad1b1e8ab3773
Shashwat <shashwatrathod22@gmail.com>
2022-10-26 16:41:52 -0400

    fix imports

--- frontend/src/components/Town/interactables/ViewingAreaVideo.tsx
+++ frontend/src/components/Town/interactables/ViewingAreaVideo.tsx
@@ -1,5 +1,5 @@
 import { Container } from '@chakra-ui/react';
-import React,{ useEffect,useRef,useState } from 'react';
+import React,{ useEffect, useRef, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { useInteractable, useViewingAreaController } from '../../../classes/TownController';
 import ViewingAreaController from '../../../classes/ViewingAreaController';
